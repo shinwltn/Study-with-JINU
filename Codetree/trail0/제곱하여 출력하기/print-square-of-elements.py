82826b9262f94d627769294a0e9bccf360cbf9c0
shinwltn <jisu3635@gmail.com>
2026-08-01 01:11:45 +0900

    [Passed] 제곱하여 출력하기 (50ms, 16MB)

---
+++ Codetree/trail0/제곱하여 출력하기/print-square-of-elements.py
@@ -0,0 +1,26 @@
+##CASE 1
+
+N = int(input())
+arr = list(map(int,input().split()))
+
+for i in arr :
+    print(i*i,end=" ") 
+
+
+##CASE 2
+
+#N = int(input())
+#arr = list(map(int, input().split()))
+
+#for i in range(N):
+#    print(arr[i] * arr[i], end=" ")
+
+##CASE 3
+
+#N = int(input())
+#arr = list(map(int, input().split()))
+
+#new_arr = [x * x for x in arr]
+
+#for x in new_arr:
+#    print(x, end=" ")
